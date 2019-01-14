0b739fec9fbefbc19e50ae7df0d851abfcda4868
vtadevosian <vahram_tadevosyan@edu.aua.am>
2019-01-14 05:08:11 +0400

    Added new registration page background image, customizations on text fields and buttons

---
+++ InYerevan/Extensions/UITextField+RoundedCustomization.swift
@@ -0,0 +1,30 @@
+//
+//  UITextField+RoundedCustomization.swift
+//  InYerevan
+//
+//  Created by Vahram Tadevosian on 1/14/19.
+//  Copyright © 2019 InYerevan.am. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func customize(by view: UIView, placeholder: String) {
+        let cornerRadius = view.bounds.height / 45
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: cornerRadius / 2, height: self.bounds.height))
+        let height = view.bounds.height / 50
+        let font = UIFont.init(name: (self.font?.fontName)!, size: height)!
+        let attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.placeholderWhite])
+        
+        self.adjustsFontSizeToFitWidth = true
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.layer.cornerRadius = cornerRadius
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.clear.cgColor
+        self.leftView = paddingView
+        self.leftViewMode = .always
+        self.font = font
+        self.attributedPlaceholder = attributedPlaceholder
+    }
+}
